e5c3154a0eb5902af8da309b8420de56748b73f2
Jesus Medina <jesus.medina05@gmail.com>
2026-06-17 13:01:14 -0400

    comment cod notification

--- medicore-backend/src/notifications/notifications.module.ts
+++ medicore-backend/src/notifications/notifications.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ScheduleModule } from '@nestjs/schedule';
 
 import { NotificationsService } from './notifications.service';
-import { NotificationsScheduler } from './notifications.scheduler';
+// import { NotificationsScheduler } from './notifications.scheduler'; // TODO: activar cuando haya dominio verificado en Resend
 import { NotificationsController } from './notifications.controller';
 
-import { User } from '../auth/entities/user.entity';
-import { Medication } from '../medications/entities/medication.entity';
-import { MedicalHistory } from '../medical-history/entities/medical-history.entity';
-import { Patient } from '../patients/entities/patient.entity';
-
 @Module({
-  imports: [
-    ScheduleModule,   // ya se registra globalmente en AppModule; aquí lo importamos para los decoradores
-    TypeOrmModule.forFeature([User, Medication, MedicalHistory, Patient]),
-  ],
+  imports: [],
   controllers: [NotificationsController],
-  providers: [NotificationsService, NotificationsScheduler],
+  providers: [NotificationsService],
   exports: [NotificationsService],
 })
 export class NotificationsModule {}
