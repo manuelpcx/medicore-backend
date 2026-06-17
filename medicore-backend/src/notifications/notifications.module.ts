57d187d6b3404e85937e36537bbf64e454c5b169
Jesus Medina <jesus.medina05@gmail.com>
2026-06-17 13:51:55 -0400

    fix: register User entity in NotificationsModule to resolve UserRepository injection

--- medicore-backend/src/notifications/notifications.module.ts
+++ medicore-backend/src/notifications/notifications.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { NotificationsService } from './notifications.service';
 // import { NotificationsScheduler } from './notifications.scheduler'; // TODO: activar cuando haya dominio verificado en Resend
 import { NotificationsController } from './notifications.controller';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
